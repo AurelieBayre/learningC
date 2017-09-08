f89045c793e8df52c1a2e9b8fabf9d5d0750cdc1
Aurelie Bayre <a.bayre@gmail.com>
2017-09-08 19:07:17 +0200

    so here is my first exercise

---
+++ learningc_001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	puts ("Hello! please press a key. Any key will do. And then press enter. Thanks!"); //print an instruction
+	
+	getchar (); //waiting for a key to be pressed
+	
+	puts ("Thank you very much!");
+	
+	return 0;
+	}
